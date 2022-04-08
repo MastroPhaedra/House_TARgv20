a874a48d90fc21955ffa9acd2aae7f31f7ae1b24
MastroPhaedra <zakharmakar1@gmail.com>
2022-04-08 16:05:13 +0300

    +Data: +HouseDbContext, Migrations

---
+++ House_TARgv20.Data/HouseDbContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using House_TARgv20.Core.Domain;
+
+namespace House_TARgv20.Data
+{
+    public class HouseDbContext : DbContext
+    {
+        public HouseDbContext(DbContextOptions<HouseDbContext> options)
+            : base(options) { }
+
+        public DbSet<HouseDomain> House { get; set; }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<HouseDomain>(entity =>
+            {
+                entity.HasKey(x => x.Id);
+            });
+
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
---
+++ House_TARgv20.Data/Migrations/20220406212538_init.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace House_TARgv20.Data.Migrations
+{
+    public partial class init : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "House",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    HouseAddress = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    ApartmentNumber = table.Column<int>(type: "int", nullable: false),
+                    FloorNumber = table.Column<int>(type: "int", nullable: false),
+                    NumberOfRooms = table.Column<int>(type: "int", nullable: false),
+                    ConditionOfTheApartment = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Price = table.Column<double>(type: "float", nullable: false),
+                    Description = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    CreatedAt = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    ModifiedAt = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_House", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "House");
+        }
+    }
+}
